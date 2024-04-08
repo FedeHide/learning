0a637fd152b820629853624f4406def2cf778e59
FedeHide <larcdian@gmail.com>
2024-04-08 19:40:54 -0300

    nextjs

--- nextjs-learning/lama-curso/src/app/page.jsx
+++ nextjs-learning/lama-curso/src/app/page.jsx
@@ -4,7 +4,7 @@ import styles from './home.module.css'
 export default function Home() {
 	return (
 		<div className={styles.container}>
-			<div className="styles.textContainer">
+			<div className={styles.textContainer}>
 				<h1 className={styles.title}>Creative Thoughts Agency</h1>
 				<p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quis consequuntur dolore voluptatum sapiente doloribus</p>
 				<div className={styles.buttons}>
@@ -15,7 +15,7 @@ export default function Home() {
 					<Image className={styles.brandImg} src='/brands.png' alt='' fill />
 				</div>
 			</div>
-			<div className="styles.imgContainer">
+			<div className={styles.imgContainer}>
 				<Image className={styles.heroImg} src='/hero.gif' alt='' fill />
 			</div>
 		</div>
