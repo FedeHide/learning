4bb2377a5efdaf245bdee0402462d729dd16935c
FedeHide <larcdian@gmail.com>
2024-04-23 13:54:43 -0300

    node

---
+++ node-learning/05.fs-asyncAwait.mjs
@@ -0,0 +1,12 @@
+import { readFile } from 'node:fs/promises';
+
+console.log('leyendo el primer archivo ...')
+
+const text = await readFile('holaMundo.txt', 'utf-8');
+console.log('primer texto:', text)
+
+console.log('--> Hacer cosas mientras lee el archivo ...')
+
+console.log('leyendo el segundo archivo ...')
+const secondText = await readFile('archivo2.txt', 'utf-8');
+console.log('segundo texto:', secondText)
