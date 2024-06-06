fef08b471afc98982d98affd65772b0e38b0278d
FedeHide <larcdian@gmail.com>
2024-06-06 13:45:08 -0300

    python

---
+++ python-learning/methods.py
@@ -0,0 +1,40 @@
+#### print()
+# The print() function prints the specified message to the screen, or other standard output device.
+print(f'print(): python!')
+
+#### type()
+# The type() function returns the type of the specified object.
+x = 5
+print(f"type(): {type(x)}")
+
+#### len()
+# The len() function returns the number of items in an object.
+my_list = [1, 2, 3, 4, 5]
+print(f"len(): {len(my_list)}")
+
+#### str()
+# The str() function returns a string representation of the specified object.
+x = 5
+print(f"str(): {str(x)}, type: {type(str(x))}")
+
+#### int()
+# The int() function returns an integer object constructed from a number or string x, or return 0 if no arguments are given.
+y = 5.5
+print(f"int(): {int(y)}, type: {type(int(y))}")
+
+#### float()
+# The float() function returns a floating-point object constructed from a number or string x.
+z = 5.5
+print(f"float(): {float(z)}, type: {type(float(z))}")
+
+#### bool()
+# The bool() function returns a boolean value.
+a = 5
+print(f"bool(): {bool(a)}, type: {type(bool(a))}")
+
+#### input()
+# The input() function reads a line from input, converts it to a string (stripping a trailing newline), and returns that.
+name = input("Enter your name: ")
+print(f"input(): {name}")
+
+
