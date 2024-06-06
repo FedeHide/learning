670ba900c143e3db504a5f1f6f2abc0774f90ed7
FedeHide <larcdian@gmail.com>
2024-06-06 13:44:41 -0300

    python

---
+++ python-learning/f-strings.py
@@ -0,0 +1,6 @@
+#### Formated strings literals
+# f-strings provide a way to embed expressions inside string literals, using curly braces {}.
+
+name = "John"
+age = 30
+print(f"Hello, My name is {name} and I am {age} years old.")
