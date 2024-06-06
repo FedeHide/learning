192c5123611dbc81fdbf2f68a7e926e6c326d593
FedeHide <larcdian@gmail.com>
2024-06-06 13:44:22 -0300

    python

---
+++ python-learning/dataTypes.py
@@ -0,0 +1,169 @@
+# Integers 
+a = 10
+
+# Float
+b = 10.5
+
+# Complex
+c = 1 + 2j
+
+# Booleans
+d = True
+
+# Strings
+e = "Hello World"
+
+# List
+f = [1, 2, 3, 4, 5]
+
+# Tuple
+g = (1, 2, 3, 4, 5)
+
+# Set
+h = {1, 2, 3, 4, 5}
+
+# Dictionary
+i = {"name": "John", "age": 30}
+
+#### Operators
+# Arithmetic Operators
+
+# Addition
+add = 10 + 5
+
+# Subtraction
+sub = 10 - 5
+
+# Multiplication
+mul = 10 * 5
+
+# Division
+div = 10 / 5
+
+# Modulus
+mod = 10 % 5
+
+# Exponentiation
+exp = 10 ** 5
+
+# Floor Division
+fdiv = 10 // 5
+
+#### Comparison Operators
+# Equal
+eq = 10 == 5
+
+# Not Equal
+neq = 10 != 5
+
+# Greater Than
+gt = 10 > 5
+
+# Less Than
+lt = 10 < 5
+
+# Greater Than or Equal To
+gte = 10 >= 5
+
+# Less Than or Equal To
+lte = 10 <= 5
+
+#### Logical Operators
+# AND
+and_op = True and False
+
+# OR
+or_op = True or False
+
+# NOT
+not_op = not True
+
+#### Identity Operators
+# is
+is_op = 10 is 5
+
+# is not
+is_not_op = 10 is not 5
+
+#### Membership Operators
+# in
+in_op = 10 in [1, 2, 3, 4, 5]
+
+# not in
+not_in_op = 10 not in [1, 2, 3, 4, 5]
+
+#### Bitwise Operators
+# AND
+bit_and = 10 & 5
+
+# OR
+bit_or = 10 | 5
+
+# XOR
+bit_xor = 10 ^ 5
+
+# NOT
+bit_not = ~10
+
+# Left Shift
+bit_lshift = 10 << 5
+
+# Right Shift
+bit_rshift = 10 >> 5
+
+#### Assignment Operators
+# =
+assign = 10
+
+# +=
+assign_add = 10
+assign_add += 5
+
+# -=
+assign_sub = 10
+assign_sub -= 5
+
+# *=
+assign_mul = 10
+assign_mul *= 5
+
+# /=
+assign_div = 10
+assign_div /= 5
+
+# %=
+assign_mod = 10
+assign_mod %= 5
+
+# **=
+assign_exp = 10
+assign_exp **= 5
+
+# //=
+assign_fdiv = 10
+assign_fdiv //= 5
+
+#### Bitwise Assignment Operators
+# &=
+assign_bit_and = 10
+assign_bit_and &= 5
+
+# |=
+assign_bit_or = 10
+assign_bit_or |= 5
+
+# ^=
+assign_bit_xor = 10
+assign_bit_xor ^= 5
+
+# ~=
+assign_bit_not = 10
+assign_bit_not = ~assign_bit_not
+
+# <<=
+assign_bit_lshift = 10
+assign_bit_lshift <<= 5
+
+# >>=
+assign_bit_rshift = 10
+assign_bit_rshift >>= 5
