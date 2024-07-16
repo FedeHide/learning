a73e1ba4a744a9ed91c61f3362e150ec127f00bf
FedeHide <larcdian@gmail.com>
2024-07-16 16:33:04 -0300

    python

--- python-learning/01-variables.py
+++ python-learning/01-variables.py
@@ -11,18 +11,18 @@ my_tuple_variable = (1, 2, 3, 4, 5)
 my_set_variable = {1, 2, 3, 4, 5}
 my_dict_variable = {"name": "John", "age": 30}
 
-print(my_dict_variable, my_set_variable)
-print(f"my_int_variable: {my_int_variable}, type: {type(my_int_variable)}")
+print(my_dict_variable, my_set_variable) # {'name': 'John', 'age': 30} {1, 2, 3, 4, 5}
+print(f"my_int_variable: {my_int_variable}, type: {type(my_int_variable)}") # my_int_variable: 5, type: <class 'int'>
 
 #### One line multiple assignment
 a, b, c = 5, 5.5, "Hello, World!"
-print(f"a: {a}, b: {b}, c: {c}")
+print(f"a: {a}, b: {b}, c: {c}") # a: 5, b: 5.5, c: Hello, World!
 
 #### Constants
 # Constants are usually declared and assigned on a module.
 PI = 3.14
 GRAVITY = 9.8
-print(f"PI: {PI}, GRAVITY: {GRAVITY}")
+print(f"PI: {PI}, GRAVITY: {GRAVITY}") # PI: 3.14, GRAVITY: 9.8
 
 
 
