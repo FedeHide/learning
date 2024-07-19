42fbed5e33b420c2466efc6d0e2b358a71d8eb88
FedeHide <larcdian@gmail.com>
2024-07-19 18:20:59 -0300

    python

---
+++ python-learning/16-exceptions.py
@@ -0,0 +1,21 @@
+### Exceptions
+# Exceptions are errors that are detected during execution. They are not unconditionally fatal, and can be handled by the program.
+# Exceptions are raised when the program encounters an error during its execution. They can be handled by the program, and can be used to prevent the program from crashing.
+
+# The try block is used to test a block of code for errors.
+# The except block is used to handle the error.
+# The else block is used to execute code if no errors are raised.
+# The finally block is used to execute code, regardless of the result of the try and except blocks.
+
+# Syntax:
+# try:
+#     # run this code
+# except:  --> may or may not have a condition
+#     # run this code when there is an exception
+# else:
+#     # no exception? run this code
+# finally:
+#     # always run this code
+
+# Example:
+
