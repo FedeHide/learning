f9b77413b056e7ea9fb8ecc243a91c4a6a16c2cf
FedeHide <larcdian@gmail.com>
2024-07-21 19:57:33 -0300

    python

--- python-learning/16-exceptions.py
+++ python-learning/16-exceptions.py
@@ -17,12 +17,15 @@
 # finally:
 #     # always run this code
 
-# Example:
 
+
+# Example:
 x = 10
+y = 5
+x = "10"
 
 try:
-    print(x)
+    print(x + y) 
 except:
     print("An exception occurred")
 
@@ -30,4 +33,31 @@ except:
 # An exception occurred
 
 
+# Example 2:
+try:
+    print(x + y)
+except TypeError: # TypeError is the type of exception that will be caught 
+    print("An exception occurred")
+except NameError: # NameError is the type of exception that will be caught
+    print("Name Error occurred")
+except:
+    print("Some other error occurred")
 
+# typeError and NameError are the types of exceptions that will be caught and the difference between the two is that the NameError is not raised in the code
+
+# Output:
+# An exception occurred
+
+
+# Example 3:
+try:
+    print(x + y)
+    print("No exception occurred")
+except:
+    print("An exception occurred")
+else:
+    print("The code continued to run")
+    # this else block will run if there is no exception
+finally:
+    print("The code has finished running")
+    # this finally block will always run
