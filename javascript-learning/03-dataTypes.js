a84742e0736213c4e89050171d6635417aa46e59
FedeHide <larcdian@gmail.com>
2024-10-17 14:48:33 -0300

    js

--- javascript-learning/03-dataTypes.js
+++ javascript-learning/03-dataTypes.js
@@ -8,7 +8,7 @@
 // They are immutable (cannot be changed) and copied by value.
 
 
-// 1.1. Number
+// 1.1. Number: is a data type that represents numeric values.
 let a = 5;
 let b = 3.14;
 let c = NaN;
@@ -16,28 +16,28 @@ let d = Infinity;
 let e = -Infinity;
 
 
-// 1.2. String
+// 1.2. String: is a data type that represents a sequence of characters.
 let f = 'JavaScript';
 let g = "Learning";
 let h = `Programming`;
 let i = 'I\'m learning JavaScript';
 
 
-// 1.3. Boolean
+// 1.3. Boolean: is a data type that can have one of two values: true or false.
 let j = true;
 let k = false;
 
 
-// 1.4. Undefined
+// 1.4. Undefined: is a variable that has been declared but not assigned a value. 
 let l;
 let m = undefined;
 
 
-// 1.5. Null
+// 1.5. Null: is a variable that has been explicitly assigned no value.
 let n = null;
 
 
-// 1.6. Symbol
+// 1.6. Symbol: is a data type that represents a unique identifier.
 let o = Symbol('symbol'); 
 
 
@@ -46,29 +46,29 @@ let o = Symbol('symbol');
 // They are mutable (can be changed) and copied by reference.
 
 
-// 2.1. Object
+// 2.1. Object: is a data type that represents a collection of key-value pairs.
 let p = { name: 'JavaScript', type: 'Programming Language' };
 
 
-// 2.2. Array
+// 2.2. Array: is a data type that represents a list of elements.
 let q = [1, 2, 3, 4, 5];
 
 
-// 2.3. Function
+// 2.3. Function: is a data type that represents a block of code that can be called.
 let r = function() {
     return 'Hello, World!';
 };
 
 
-// 2.4. Date
+// 2.4. Date: is a data type that represents a date and time.
 let s = new Date();
 
 
-// 2.5. RegExp
+// 2.5. RegExp: is a data type that represents a regular expression.
 let t = /\w+/;
 
 
-// 2.6. Error
+// 2.6. Error: is a data type that represents an error object.
 let u = new Error('Error Message');
 
 
