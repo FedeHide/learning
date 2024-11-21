dac453b74e9305ff061d139cf28e869d0f1205ad
FedeHide <larcdian@gmail.com>
2024-11-21 20:58:47 -0300

    python

--- python-learning/16_exceptions.py
+++ python-learning/16_exceptions.py
@@ -7,7 +7,7 @@
 # The else block is used to execute code if no errors are raised.
 # The finally block is used to execute code, regardless of the result of the try and except blocks.
 
-# Syntax:
+# * Syntax:
 # try:
 #     # run this code
 # except:  --> may or may not have a condition
@@ -42,7 +42,7 @@ except NameError:  # NameError is the type of exception that will be caught
 except:
     print("Example 2: Some other error occurred")
 
-# typeError and NameError are the types of exceptions that will be caught and the difference between the two is that the NameError is not raised in the code
+# * typeError and NameError are the types of exceptions that will be caught and the difference between the two is that the NameError is not raised in the code
 
 # Output:
 # An exception occurred
@@ -62,7 +62,7 @@ finally:  # optional
     # this finally block will always run
 
 
-### Except errors list:
+###* Except errors list:
 # ArithmeticError - Base class for math-related errors.
 # AssertionError - Raised when an assert statement fails.
 # AttributeError - Raised when an attribute reference or assignment fails.
