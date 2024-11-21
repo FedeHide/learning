be5638d2c8abfae865d068563bce38a1487e3f92
FedeHide <larcdian@gmail.com>
2024-11-21 19:50:37 -0300

    python

---
+++ python-learning/challenges/inverted_string.py
@@ -0,0 +1,9 @@
+## inverted string challenge
+# Write a function that takes a string as input and returns the string reversed.
+
+
+def inverted_string(string: str) -> str:
+    return string[::-1]
+
+
+print(inverted_string("Hello World"))
