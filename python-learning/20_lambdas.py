eb5d0d8d00dade21fbf567c5f8f5a876b1a00bf1
FedeHide <larcdian@gmail.com>
2024-11-21 20:34:46 -0300

    python

---
+++ python-learning/20_lambdas.py
@@ -0,0 +1,15 @@
+### Lambdas
+# Lambdas are a way to create small anonymous functions, i.e. functions without a name.
+# They are temporary functions that are needed only once.
+# a lambda function can take any number of arguments, but can only have one expression.
+
+# Syntax:
+# lambda arguments : expression
+
+# Example:
+x = lambda a: a + 10
+print(x(5))
+
+# Example 2:
+y = lambda a, b: a * b
+print(y(5, 6))
