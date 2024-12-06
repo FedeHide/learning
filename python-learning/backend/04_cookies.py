a3d80f3733a8faf49b31fb0a6ca72105609aecf3
FedeHide <larcdian@gmail.com>
2024-12-05 21:28:26 -0300

    python

---
+++ python-learning/backend/04_cookies.py
@@ -0,0 +1,4 @@
+### cookies
+# cookies are small pieces of data that are stored in the user's browser
+# cookies are used to store user data like session id, user id, etc.
+# cookies are sent to the server with every request.
