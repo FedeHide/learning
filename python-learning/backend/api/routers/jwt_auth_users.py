65b1504c50600708469c4c97ca30f1ee847cd344
FedeHide <larcdian@gmail.com>
2024-12-06 20:15:52 -0300

    python

--- python-learning/backend/api/routers/jwt_auth_users.py
+++ python-learning/backend/api/routers/jwt_auth_users.py
@@ -136,14 +136,7 @@ async def get_current_active_user(
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
 ) -> Token:
-    ## Depends() is used to inject the dependency into the path operation function. Allows you to declare the dependency directly in the path operation function.
     user = authenticate_user(users_db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
