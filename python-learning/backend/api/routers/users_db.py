3497cf4c1346dc07ca4506b56f1bef60d4af5b41
FedeHide <larcdian@gmail.com>
2024-12-15 18:35:46 -0300

    python

--- python-learning/backend/api/routers/users_db.py
+++ python-learning/backend/api/routers/users_db.py
@@ -11,8 +11,7 @@ router = APIRouter(
     prefix="/usersdb", tags=["usersdb"], responses={404: {"description": "Not found"}}
 )
 
-
-users_list: list[User] = []
+db_collection = db_client.local.users
 
 
 def handle_pymongo_error(error: Exception, context: str):
@@ -68,9 +67,11 @@ def get_user_by_email_or_username(
 
 
 # TODO: refactor the code from here
-@router.get("/", response_model=List[User])
+@router.get("/")
 async def read_users():
-    return users_list
+    users_list = db_collection.find()
+    parsed_users = [user_schema(user) for user in users_list]
+    return [User(**user) for user in parsed_users]
 
 
 ## ? Query Parameters
@@ -78,41 +79,41 @@ async def read_users():
 # * Query parameters are defined in the URL path using a question mark ? followed by the key-value pair.
 
 
-@router.get("/search", response_model=List[User])
-async def read_users_by_query(
-    id: Optional[int] = Query(None), name: Optional[str] = Query(None)
-):
-    # check if at least one query parameter is provided
-    if id is None and name is None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="At least one query parameter ('id' or 'name') is required",
-        )
-    # filter users based on query parameters
-    filtered_users = users_list
-    if id is not None:
-        filtered_users = [user for user in filtered_users if user.id == id]
-    if name is not None:
-        filtered_users = [user for user in filtered_users if user.name == name]
-    if not filtered_users:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
-    return filtered_users
+# @router.get("/search", response_model=List[User])
+# async def read_users_by_query(
+#     id: Optional[int] = Query(None), name: Optional[str] = Query(None)
+# ):
+#     # check if at least one query parameter is provided
+#     if id is None and name is None:
+#         raise HTTPException(
+#             status_code=status.HTTP_400_BAD_REQUEST,
+#             detail="At least one query parameter ('id' or 'name') is required",
+#         )
+#     # filter users based on query parameters
+#     filtered_users = users_list
+#     if id is not None:
+#         filtered_users = [user for user in filtered_users if user.id == id]
+#     if name is not None:
+#         filtered_users = [user for user in filtered_users if user.name == name]
+#     if not filtered_users:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+#         )
+#     return filtered_users
 
 
 ## ? Path Parameters
 # * Path parameters are used to pass data to the server. The data is passed in the URL itself.
 # * Path parameters are defined in the URL path using curly braces {parameter_name}.
-@router.get("/{user_id}", response_model=User)
-async def read_user_by_path(user_id: int):
-    user = next((user for user in users_list if user.id == user_id), None)
-    # ? next(generator, default)
-    # generator: A generator that produces values
-    # default: A default value to return if the generator is exhausted
-    if user:
-        return user
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+# @router.get("/{user_id}", response_model=User)
+# async def read_user_by_path(user_id: int):
+#     user = next((user for user in users_list if user.id == user_id), None)
+#     # ? next(generator, default)
+#     # generator: A generator that produces values
+#     # default: A default value to return if the generator is exhausted
+#     if user:
+#         return user
+#     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
 
 # TODO to here
@@ -130,7 +131,6 @@ async def create_user(user: Annotated[User, "User data"]):
     Returns:
         User: The created user data.
     """
-    db_collection = db_client.local.users
 
     # Check if the email or username is already registered in the database
     get_user_by_email_or_username(user.email, user.username, db_collection)
@@ -161,38 +161,38 @@ async def create_user(user: Annotated[User, "User data"]):
 
 
 # TODO: refactor the code from here
-@router.put("/{user_id}")
-async def update_user(user_id: int, user: Annotated[User, "User data"]):
-    # Check if the email is already registered or is the same as the user's email
-    if any(
-        existing_user.email == user.email
-        for existing_user in users_list
-        if existing_user.id != user_id
-    ):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="User with this email already exists",
-        )
+# @router.put("/{user_id}")
+# async def update_user(user_id: int, user: Annotated[User, "User data"]):
+#     # Check if the email is already registered or is the same as the user's email
+#     if any(
+#         existing_user.email == user.email
+#         for existing_user in users_list
+#         if existing_user.id != user_id
+#     ):
+#         raise HTTPException(
+#             status_code=status.HTTP_400_BAD_REQUEST,
+#             detail="User with this email already exists",
+#         )
 
-    for index, existing_user in enumerate(users_list):
-        if existing_user.id == user_id:
-            users_list[index] = user.model_copy(update={"id": existing_user.id})
-            # ? model_copy() method is used to create a copy of the model with the specified updates.
-            return {"message": "User updated successfully", "user": users_list[index]}
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+#     for index, existing_user in enumerate(users_list):
+#         if existing_user.id == user_id:
+#             users_list[index] = user.model_copy(update={"id": existing_user.id})
+#             # ? model_copy() method is used to create a copy of the model with the specified updates.
+#             return {"message": "User updated successfully", "user": users_list[index]}
+#     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
 
-@router.delete("/{user_id}")
-async def delete_user(user_id: int):
-    user_to_delete = next((user for user in users_list if user.id == user_id), None)
+# @router.delete("/{user_id}")
+# async def delete_user(user_id: int):
+#     user_to_delete = next((user for user in users_list if user.id == user_id), None)
 
-    if user_to_delete is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
-        )
+#     if user_to_delete is None:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+#         )
 
-    users_list.remove(user_to_delete)
-    return {"message": "User deleted successfully", "user": user_to_delete}
+#     users_list.remove(user_to_delete)
+#     return {"message": "User deleted successfully", "user": user_to_delete}
 
 
 # TODO to here
