ae3b4071bc532b41bc2965b9d82f1ec505afb398
FedeHide <larcdian@gmail.com>
2024-12-16 13:15:50 -0300

    python

--- python-learning/backend/api/db/connection.py
+++ python-learning/backend/api/db/connection.py
@@ -8,4 +8,4 @@ from pymongo import MongoClient
 # remote client
 db_client = MongoClient(
     "mongodb+srv://test:9a6ZKNyVytVQo1FF@clustertest.vmiea.mongodb.net/?retryWrites=true&w=majority&appName=ClusterTest"
-)
+).test
