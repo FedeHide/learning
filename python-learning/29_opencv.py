4a8ca035caed8e052f77f5839dcd4351b8bd2810
FedeHide <larcdian@gmail.com>
2025-01-13 19:13:28 -0300

    python

--- python-learning/29_opencv.py
+++ python-learning/29_opencv.py
@@ -9,9 +9,7 @@ import cv2
 # * imread() - read an image
 # * parameters: image path, flag
 # * flag: 0 - grayscale, 1 - color, -1 - alpha channel
-img = cv2.imread(
-    "schemas/galaxy.jpg", 1
-)  # parameters: 0 - grayscale, 1 - color, -1 - alpha channel
+img = cv2.imread("schemas/galaxy.jpg", 1)
 
 
 # * what is the img?
@@ -25,19 +23,22 @@ print(img.size)  # 4407300 - total number of pixels
 
 # * resize() - resize an image
 # * parameters: image, (width, height)
-resized_img = cv2.resize(
-    img, (int(img.shape[1] / 2), int(img.shape[0] / 2))
-)  # parameters: image, (width, height)
+resized_img = cv2.resize(img, (int(img.shape[1] / 2), int(img.shape[0] / 2)))
 
 
 # * imshow() - display an image
 # * parameters: window name, image
-cv2.imshow("Galaxy", resized_img)  # parameters: window name, image
+cv2.imshow("Galaxy", resized_img)
+
+
+# * imwrite() - write an image
+# * parameters: image path, image
+cv2.imwrite("schemas/galaxy_resized.jpg", resized_img)
 
 
 # * waitKey() - wait for a key event
 # * parameter: time in milliseconds (0 - infinite)
-cv2.waitKey(0)  # parameter: time in milliseconds (0 - infinite)
+cv2.waitKey(0)
 
 
 # * destroyAllWindows() - destroy all windows
