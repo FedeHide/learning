cc50b7cb8cbbe540dd5adcffa68fea71b0e96ce9
FedeHide <larcdian@gmail.com>
2025-01-13 20:51:38 -0300

    python

--- python-learning/29_opencv.py
+++ python-learning/29_opencv.py
@@ -7,34 +7,34 @@
 import cv2
 import os
 
+"""
 # * imread() - read an image
 # * parameters: image path, flag
 # * flag: 0 - grayscale, 1 - color, -1 - alpha channel
-img = cv2.imread("schemas/galaxy.jpg", 1)
+galaxy_img = cv2.imread("schemas/galaxy.jpg", 1)
 
 
 # * what is the img?
 
-print(type(img))  # <class 'numpy.ndarray'>
-print(img)
-print(img.shape)  # (1485, 990, 3) - 1485 rows, 990 columns, 3 channels (RGB)
-print(img.ndim)  # 3 - 3 channels (RGB)
-print(img.size)  # 4407300 - total number of pixels
-
+print(type(galaxy_img))  # <class 'numpy.ndarray'>
+print(galaxy_img)
+print(galaxy_img.shape)  # (1485, 990, 3) - 1485 rows, 990 columns, 3 channels (RGB)
+print(galaxy_img.ndim)  # 3 - 3 channels (RGB)
+print(galaxy_img.size)  # 4407300 - total number of pixels
 
 # * resize() - resize an image
 # * parameters: image, (width, height)
-resized_img = cv2.resize(img, (int(img.shape[1] / 2), int(img.shape[0] / 2)))
+galaxy_resized_img = cv2.resize(img, (int(img.shape[1] / 2), int(img.shape[0] / 2)))
 
 
 # * imshow() - display an image
 # * parameters: window name, image
-cv2.imshow("Galaxy", resized_img)
+cv2.imshow("Galaxy", galaxy_resized_img)
 
 
 # * imwrite() - write an image
 # * parameters: image path, image
-cv2.imwrite("schemas/galaxy_resized100.jpg", resized_img)
+cv2.imwrite("schemas/galaxy_resized100.jpg", galaxy_resized_img)
 
 
 # * waitKey() - wait for a key event
@@ -44,6 +44,7 @@ cv2.waitKey(0)
 
 # * destroyAllWindows() - destroy all windows
 cv2.destroyAllWindows()
+"""
 
 
 # ! create a function to resize all images in a folder to 100x100
@@ -55,13 +56,13 @@ def resize_all_images_to_100x100(folder):
         cv2.imwrite(f"{folder}/resized-{file}", resized_img)
 
 
-resize_all_images_to_100x100("schemas/sample_images")
+# resize_all_images_to_100x100("schemas/sample_images")
 
 
 # ! same function with glob to find all images in a folder
 import glob
 
-# * glob - find all the pathnames matching a specified pattern according to the rules used by the Unix shell
+# ? glob - find all the pathnames matching a specified pattern according to the rules used by the Unix shell
 # * glob.glob() - returns a list of pathnames matching the pattern
 # * parameters: pattern
 # * pattern: * - zero or more characters, ? - single character
@@ -94,3 +95,58 @@ def resize_all_images_to_100x100_glob(folder):
             print(f"Image {file} resized successfully.")
         except Exception as e:
             print(f"Error to resize image {file}: {e}")
+
+
+# resize_all_images_to_100x100_glob("schemas/sample_images")
+
+
+# ? face detection - haarcascades
+# * haarcascades - pre-trained classifiers for detecting objects
+# * haarcascades_frontalface_default.xml - pre-trained classifier for face detection
+# * haarcascades_eye.xml - pre-trained classifier for eye detection
+
+
+# * CascadeClassifier() - create a cascade classifier to detect objects
+# * parameters: classifier path
+# * classifier path: path to the pre-trained classifier
+# * returns: cascade classifier object
+face_cascade = cv2.CascadeClassifier(
+    "schemas/face_detection/haarcascade_frontalface_default.xml"
+)
+
+# load the image
+face_img = cv2.imread("schemas/face_detection/photo.jpg")
+
+
+# * cvtColor() - convert an image to another color space
+# * parameters: image, color space
+# * color space: cv2.COLOR_BGR2GRAY, cv2.COLOR_BGR2RGB, cv2.COLOR_BGR2HSV, cv2.COLOR_BGR2LAB, cv2.COLOR_BGR2YUV, cv2.COLOR_BGR2XYZ ...
+face_gray_img = cv2.cvtColor(face_img, cv2.COLOR_BGR2GRAY)
+#! passing 0 to imread() to read the image in grayscale improves the performance of the face detection
+
+
+# * detectMultiScale() - detect objects in an image
+# * parameters: image, scaleFactor, minNeighbors
+# * scaleFactor: parameter specifying how much the image size is reduced at each image scale
+# * minNeighbors: parameter specifying how many neighbors each candidate rectangle should have to retain it
+faces = face_cascade.detectMultiScale(face_gray_img, scaleFactor=1.05, minNeighbors=5)
+
+print(faces)  # [(x, y, width, height), ...]
+print(type(faces))  # <class 'numpy.ndarray'>
+print(faces.ndim)  # 2
+print(faces.shape)  # (1, 4) - 1 face, 4 coordinates
+
+
+# * rectangle() - draw a rectangle on an image
+# * parameters: image, start_point, end_point, color, thickness
+# * start_point: top-left corner of the rectangle
+# * end_point: bottom-right corner of the rectangle
+# * color: rectangle color
+# * thickness: rectangle thickness
+for x, y, w, h in faces:
+    face_img = cv2.rectangle(face_img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+
+cv2.imshow("Face Detection", face_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
