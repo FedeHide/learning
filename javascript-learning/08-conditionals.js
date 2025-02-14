80818a2e145eed616cf5a4c0f989a705a27afe00
FedeHide <larcdian@gmail.com>
2025-02-14 20:53:52 -0300

    js

--- javascript-learning/08-conditionals.js
+++ javascript-learning/08-conditionals.js
@@ -1,29 +1,29 @@
 //?/ Conditionals: are used to execute a block of code based on a condition.
 
 
-//*/ 1. if statement: executes a block of code if a specified condition is true.
 let x = 10;
 
+//*/ 1. if statement: executes a block of code if a specified condition is true.
 if (x > 5) {
-    console.log('x is greater than 5');
+    console.log("if: ", 'x is greater than 5');
 }
 
 
 //*/ 2. else statement: executes a block of code if the same condition is false.
-if (x < 5) {
-    console.log('x is less than 5');
+if (x < 11) {
+    console.log("else: ", 'x is less than 11');
 } else {
-    console.log('x is greater than 5');
+    console.log("else: ", 'x is greater than 11');
 }
 
 
 //*/ 3. else if statement: specifies a new condition if the first condition is false.
 if (x < 5) {
-    console.log('x is less than 5'); 
+    console.log("else if: ", 'x is less than 5'); 
 } else if (x > 5) { 
-    console.log('x is greater than 5'); 
+    console.log("else if: ", 'x is greater than 5'); 
 } else {
-    console.log('x is equal to 5');
+    console.log("else if: ", 'x is equal to 5');
 }
 
 
@@ -57,11 +57,11 @@ switch (day) {
         dayName = 'Invalid day';
 }
 
-console.log(dayName); // Output: Wednesday
+console.log("switch: ", dayName); // Output: Wednesday
 
 
 //*/ 5. ternary operator: assigns a value to a variable based on a condition.
-let age = 20;
+let age = 22;
 let beverage = (age >= 21) ? 'Beer' : 'Juice';
-console.log(beverage); // Output: Beer
+console.log("ternary operator: ", beverage); // Outpu: Beer
 
