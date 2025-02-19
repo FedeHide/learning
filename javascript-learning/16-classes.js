8104e697c184eeba09b39e6166e99a00dcba5103
FedeHide <larcdian@gmail.com>
2025-02-19 13:16:00 -0300

    js

--- javascript-learning/16-classes.js
+++ javascript-learning/16-classes.js
@@ -1,7 +1,7 @@
 //?/ Classes: are a blueprint for creating objects with pre-defined properties and methods.
 
 
-//*/ 1. Class Declaration: defines a named class.
+//*/ Class Declaration: defines a named class.
 console.log("\n", "1. Class Declaration");
 
 class User {
@@ -11,11 +11,26 @@ class User {
     constructor(name, age, bankAccount) { // Constructor: method for creating and initializing an object created with a class.
         this.name = name; // this: refers to the current object.
         this.age = age;
-        this.#bankAccount = bankAccount;
+        this.#bankAccount = bankAccount; // Private Property: assigns a value to a private property.
     }
+
+    //*/ getters and setters: are used to get and set the values of an object's properties.
+    get bankAccount() { // Getter: defines a getter for the class.
+        return this.#bankAccount;
+    }
+
+    set bankAccount(value) { // Setter: defines a setter for the class.
+        this.#bankAccount = value;
+    }
+
+    //*/ Methods: are functions that are defined within a class.
 
     obtainInformation() { // Method: defines a method for the class.
         console.log(`Name: ${this.name}, Age: ${this.age}`);
+    }
+
+    work() { // Method: defines a method for the class.
+        console.log(`${this.name} is working.`);
     }
 }
 
@@ -33,6 +48,11 @@ class Admin extends User { // Inheritance: creates a class based on another clas
         }
     }
 
+    work() { // Method: defines a method for the class.
+        super.work(); // super: calls the parent class method.
+        console.log(`${this.name} is an admin.`);
+    }
+
     static deleteUser(id) { // Static Method: defines a method that belongs to the class itself.
         console.log(`User ${id} has been deleted.`); 
     }
@@ -42,6 +62,16 @@ const user = new User('John', 30); // new: creates an instance of a class.
 const admin = new Admin('Jane', 25, ['delete']); // new: creates an instance of a class
 
 user.obtainInformation(); // Output: Name: John, Age: 30
+user.work(); // Output: John is working.
+console.log("User Type:", user.type); // Output: User
+console.log("Admin Register:", Admin.register); // Output: Admin
 admin.deletePost(5); // Output: Post 5 has been deleted.
 Admin.deleteUser(1); // Output: User 1 has been deleted.
+admin.work(); // Output: Jane is working. Jane is an admin.
 
+const user2 = new User('John', 30, '654321'); // new: creates an instance of a class.
+console.log("get Bank Account:", user2.bankAccount); // Output: Bank Account: 654321 //! it only works with the getter.
+
+// change the value of the private property with setter.
+user2.bankAccount = '123456'; //! this only works with the setter.
+console.log("set Bank Account:", user2.bankAccount); // Output: Bank Account: 123456 
