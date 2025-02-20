8b2c6db456935aaa4436e0f0c5abfd5b1df4cbd6
FedeHide <larcdian@gmail.com>
2025-02-20 20:40:06 -0300

    js

--- javascript-learning/18-json.js
+++ javascript-learning/18-json.js
@@ -1,6 +1,4 @@
 //?/ Json: JavaScript Object Notation is a lightweight data-interchange format. It is easy for humans to read and write. 
-//?/ It is easy for machines to parse and generate.
-//?/ It is based on a subset of the JavaScript Programming Language, Standard ECMA-262 3rd Edition - December 1999.
 
 //*/ Types of data that we can use in JSON:
 //- text string
@@ -10,44 +8,61 @@
 //- booleans
 //- null
 
-//Types of data that we CANNOT use in JSON:
+//! Types of data that we CANNOT use in JSON:
 //- functions
 //- date objects
 //- undefined
 
+//*/ JSON Syntax Rules
+//- Data is in name/value pairs
+//- Data is separated by commas
+//- Curly braces hold objects
+//- Square brackets hold arrays
 
-//*/ Methods to work with JSON
 
-//*/ parse(): we pass it a text string in JSON format and it returns a JavaScript object.
-const datos = `
-{
-    "id": 1,
-    "name": "ruki",
-    "active_subscriber": true,
-    "posts": [
-        {
-            "id": 1,
-            "title": "Title of the first post",
-            "text": "text of the first post ..."
-        },
-        {
-            "id": 2,
-            "title": "Title of the second post",
-            "text": "text of the second post ..."
-        }
-    ]
-}
-`;
-const object = JSON.parse(datos); // parse: convert the JSON string to a JavaScript object
-console.log(object.name);
+//*/ import JSON from a file
+import { readFileSync } from 'fs';
+const data = readFileSync('18b-json.json', 'utf8'); // readFileSync: read the content of the file
+console.log("data: ", data); 
+
+
+// //*/ parse(): we pass it a text string in JSON format and it returns a JavaScript object.
+console.log("\n", "JSON Parse: ");
+
+const object = JSON.parse(data); // parse: convert the JSON string to a JavaScript object
+console.log("parse (JSON -> object): ", object); 
 
 
 //*/ stringify(): allows us to pass a JavaScript object and it returns a text string with the object in JSON format
+console.log("\n", "JSON Object: ");
+
 const user = {
     name: 'Ruki',
-    email: 'elpurrigmail.com',
+    email: 'elpurri@gmail.com',
     favoriteColors: ['blue', 'red', 'black'],
 };
 
 const userJson = JSON.stringify(user); // stringify: convert the JavaScript object to a JSON string
-console.log(userJson); // Output: {"name":"Ruki","email":"elpurrigmail.com","favoriteColors":["blue","red","black"]}
+console.log("user (object): ", user); 
+console.log("stringify (object -> json): ", userJson); 
+
+
+// json array: is an array that contains JSON objects
+console.log("\n", "JSON Array: ");
+
+const users = [
+    {
+        name: 'Ruki',
+        email: 'elpurri@gmail.com',
+        favoriteColors: ['blue', 'red', 'black'],
+    },
+    {
+        name: 'Anya',
+        email: 'nanitas@gmail.com',
+        favoriteColors: ['green', 'yellow', 'white'],
+    },
+];
+
+const usersJson = JSON.stringify(users); // stringify: convert the JavaScript object to a JSON string
+console.log("users (object): ", users);
+console.log("stringify (object -> json): ", usersJson);
