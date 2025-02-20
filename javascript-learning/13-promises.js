9486d1b70e62a66ae335789e62dc3f3ef8cbef0a
FedeHide <larcdian@gmail.com>
2025-02-20 20:54:24 -0300

    js

--- javascript-learning/13-promises.js
+++ javascript-learning/13-promises.js
@@ -1,12 +1,13 @@
 //?/ Promises: are a way to handle asynchronous operations in JavaScript.
-//?/ They are easy to manage when dealing with multiple asynchronous operations where callbacks can create callback hell leading to unmanageable code. 
-//?/ Promises provide a cleaner way to handle asynchronous operations.
-//?/ A Promise is an object representing the eventual completion or failure of an asynchronous operation.
-//?/ A Promise is in one of these states:
-//?/ - pending: initial state, neither fulfilled nor rejected.
-//?/ - fulfilled: meaning that the operation completed successfully.
-//?/ - rejected: meaning that the operation failed.
-//?/ Syntax:
+
+//*/ A Promise is in one of these states:
+// - pending: initial state, neither fulfilled nor rejected.
+// - fulfilled: meaning that the operation completed successfully.
+// - rejected: meaning that the operation failed.
+
+
+
+//*/ Syntax:
 
 let promise = new Promise((resolve, reject) => {
     fetch('https://jsonplaceholder.typicode.com/users/1')
@@ -20,5 +21,9 @@ promise
     .then(data => console.log(data))
     .catch(error => console.error(error))
     .finally(() => console.log('Data fetched!'));
-//?/ Output: [ { id: 1, name: 'Leanne Graham', ... }, ... ]
-//?/ Output: Data fetched!
+
+//*/ Methods:
+// - Promise.all(): waits for all promises to be resolved.
+// - Promise.allSettled(): waits for all promises to be settled.
+// - return Promise.resolve(): returns a resolved Promise.
+// - return Promise.reject(): returns a rejected Promise.
