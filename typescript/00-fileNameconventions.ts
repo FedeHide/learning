607bb8a7c22c3eed3f9353297d7243eb94e630d0
FedeHide <larcdian@gmail.com>
2025-03-07 12:58:42 -0300

    ts

---
+++ typescript/00-fileNameconventions.ts
@@ -0,0 +1,15 @@
+//? File name conventions in TypeScript / react
+
+// React components - PascalCase //! e.g. UserProfile.tsx
+// Hooks - useCamelCase //! e.g. useCounter.ts
+// Folders - kebab-case //! e.g. my-component
+// Constants - UPPERCASE //! e.g. API_URL
+// Content providers - PascalCase //! e.g. UserContext.tsx
+// Utilities - camelCase //! e.g. formatString.ts
+// Interfaces - PascalCase //! e.g. UserInterface.ts or IUser.ts
+// Types - PascalCase //! e.g. UserTypes.ts
+// Enums - PascalCase //! e.g. UserEnum.ts
+// Functions - camelCase //! e.g. getUser.ts
+// config files - PascalCase //! e.g. AppConfig.ts
+// Services - PascalCase //! e.g. UserService.ts
+// Classes - PascalCase //! e.g. UserClass.ts
